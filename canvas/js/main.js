7a9da569cb2ded34b13f1cc45db1e61dedd18440
mark <197100724@qq.com>
2019-06-24 22:41:46 +0800

    simple canvas

---
+++ canvas/js/main.js
@@ -0,0 +1,66 @@
+var canvas = document.getElementById('xxx')
+var context = canvas.getContext('2d')
+
+/* 定义canvas的宽高 */
+setCanvasSize()
+window.onresize = function () {
+    setCanvasSize()
+}
+function setCanvasSize() {/* 定义Canvas的宽高 */
+    var pageWidth = document.documentElement.clientWidth
+    var pageHeight = document.documentElement.clientHeight
+    canvas.width = pageWidth
+    canvas.height = pageHeight
+}
+
+/* 通过点击控制 画画和橡皮擦功能 */
+var eraserEnabled = false
+eraser.onclick = function(){
+    eraserEnabled = true
+    actions.className = 'actions x'
+}
+brush.onclick = function(){
+    eraserEnabled = false
+    actions.className = 'actions'
+}
+
+
+var using = false
+canvas.onmousedown = function (aaa) {
+    x = aaa.clientX
+    y = aaa.clientY
+    using = true/* 鼠标按下时 画画 或 擦除 */
+    if (eraserEnabled) {
+        context.clearRect(x, y, 10, 10)
+    } else {
+        context.beginPath()
+        context.strokeStyle = 'red'
+        context.lineWidth = 10
+        context.arc(x, y, 1, 0, 8, true)
+        context.stroke()
+        context.closePath()
+    }
+}
+canvas.onmousemove = function (aaa) {
+    var x2 = aaa.clientX
+    var y2 = aaa.clientY
+    console.log(using)
+    if(eraserEnabled && using){/* 橡皮擦被按下 且 鼠标被按下 才可以清除 if(!using){return}*/
+        context.clearRect(x2, y2,10, 10)
+    }else{
+        if (using) {
+            context.moveTo(x, y)
+            context.lineTo(x2, y2)
+            /* context.arc(x2, y2, 1, 0, 8, true) */
+            context.stroke()
+            context.closePath()
+            x = x2
+            y = y2
+    }
+    }
+}
+canvas.onmouseup = function () {
+    using = false
+}
+/* 按钮被选中 */
+
