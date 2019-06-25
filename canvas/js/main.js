ed19db195452d6d3366e58151c5aba62c12a5c97
mark <197100724@qq.com>
2019-06-25 20:42:17 +0800

    增加手机触屏使用画板

--- canvas/js/main.js
+++ canvas/js/main.js
@@ -12,55 +12,97 @@ function setCanvasSize() {/* 定义Canvas的宽高 */
     canvas.width = pageWidth
     canvas.height = pageHeight
 }
+    /* 通过点击控制 画画和橡皮擦功能 */
 
-/* 通过点击控制 画画和橡皮擦功能 */
 var eraserEnabled = false
-eraser.onclick = function(){
-    eraserEnabled = true
-    actions.className = 'actions x'
-}
-brush.onclick = function(){
-    eraserEnabled = false
-    actions.className = 'actions'
-}
-
-
-var using = false
-canvas.onmousedown = function (aaa) {
-    x = aaa.clientX
-    y = aaa.clientY
-    using = true/* 鼠标按下时 画画 或 擦除 */
-    if (eraserEnabled) {
-        context.clearRect(x, y, 10, 10)
-    } else {
-        context.beginPath()
-        context.strokeStyle = 'red'
-        context.lineWidth = 10
-        context.arc(x, y, 1, 0, 8, true)
-        context.stroke()
-        context.closePath()
+    eraser.onclick = function () {
+        eraserEnabled = true
+        actions.className = 'actions x'
     }
-}
-canvas.onmousemove = function (aaa) {
-    var x2 = aaa.clientX
-    var y2 = aaa.clientY
-    console.log(using)
-    if(eraserEnabled && using){/* 橡皮擦被按下 且 鼠标被按下 才可以清除 if(!using){return}*/
-        context.clearRect(x2, y2,10, 10)
-    }else{
-        if (using) {
-            context.moveTo(x, y)
-            context.lineTo(x2, y2)
-            /* context.arc(x2, y2, 1, 0, 8, true) */
+    brush.onclick = function () {
+        eraserEnabled = false
+        actions.className = 'actions'
+    }
+
+/* 检测鼠标设备或者触屏设备 */
+if (document.body.ontouchstart !== undefined) {
+    var using = false
+    canvas.ontouchstart = function (aaa) {
+        x = aaa.touches[0].clientX
+        y = aaa.touches[0].clientY
+        using = true/* 鼠标按下时 画画 或 擦除 */
+        if (eraserEnabled) {
+            context.clearRect(x, y, 10, 10)
+        } else {
+            context.beginPath()
+            context.strokeStyle = 'red'
+            context.lineWidth = 10
+            context.arc(x, y, 1, 0, 8, true)
             context.stroke()
             context.closePath()
-            x = x2
-            y = y2
+        }
     }
+    canvas.ontouchmove = function (aaa) {
+        x2 = aaa.touches[0].clientX
+        y2 = aaa.touches[0].clientY
+        if (eraserEnabled && using) {/* 橡皮擦被按下 且 鼠标被按下 才可以清除 if(!using){return}*/
+            context.clearRect(x2, y2, 10, 10)
+        } else {
+            if (using) {
+                context.moveTo(x, y)
+                context.lineTo(x2, y2)
+                /* context.arc(x2, y2, 1, 0, 8, true) */
+                context.stroke()
+                context.closePath()
+                x = x2
+                y = y2
+            }
+        }
+    }
+    canvas.ontouchend = function () {
+        using = false
+    }
+} else {
+    var using = false
+    canvas.onmousedown = function (aaa) {
+        x = aaa.clientX
+        y = aaa.clientY
+        using = true/* 鼠标按下时 画画 或 擦除 */
+        if (eraserEnabled) {
+            context.clearRect(x, y, 10, 10)
+        } else {
+            context.beginPath()
+            context.strokeStyle = 'red'
+            context.lineWidth = 10
+            context.arc(x, y, 1, 0, 8, true)
+            context.stroke()
+            context.closePath()
+        }
+    }
+    canvas.onmousemove = function (aaa) {
+        var x2 = aaa.clientX
+        var y2 = aaa.clientY
+        if (eraserEnabled && using) {/* 橡皮擦被按下 且 鼠标被按下 才可以清除 if(!using){return}*/
+            context.clearRect(x2, y2, 10, 10)
+        } else {
+            if (using) {
+                context.moveTo(x, y)
+                context.lineTo(x2, y2)
+                /* context.arc(x2, y2, 1, 0, 8, true) */
+                context.stroke()
+                context.closePath()
+                x = x2
+                y = y2
+            }
+        }
+    }
+    canvas.onmouseup = function () {
+        using = false
     }
 }
-canvas.onmouseup = function () {
-    using = false
-}
+
+
+
+
 /* 按钮被选中 */
 
